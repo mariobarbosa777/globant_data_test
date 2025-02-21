e7bc0b73f73b3df21c169e7a9be49e6eb8b9a004
mario barbosa <mario.barbosa@bhp.com>
2025-02-20 23:04:32 -0500

    replace volumen with copy

---
+++ db/insert_historical_data.sql
@@ -0,0 +1,20 @@
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM departments) THEN
+        COPY departments(id, department)
+        FROM '/docker-entrypoint-initdb.d/departments.csv'
+        WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM jobs) THEN
+        COPY jobs(id, job)
+        FROM '/docker-entrypoint-initdb.d/jobs.csv'
+        WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM hired_employees) THEN
+        COPY hired_employees(id, name, datetime, department_id, job_id)
+        FROM '/docker-entrypoint-initdb.d/hired_employees.csv'
+        WITH (FORMAT csv, HEADER true, DELIMITER ',');
+    END IF;
+END $$;
